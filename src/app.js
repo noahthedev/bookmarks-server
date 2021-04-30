2772a2ff9d96b30e1e142d4e0c48dd7281d91dc6
Noah Roberts <noahmroberts@gmail.com>
2021-04-30 14:31:31 -0500

    added store

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
 const logger = require('./logger')
-//const bookmarksRouter = require('./bookmarks/router')
+const bookmarksRouter = require('./bookmarks-router')
 
 const app = express()
 
@@ -25,7 +25,7 @@ app.use(function validateBearerToken(req, res, next) {
   next()
 })
 
-//app.use(bookmarksRouter)
+app.use(bookmarksRouter)
 
 app.use(function errorHandler(error, req, res, next) {
   let response
